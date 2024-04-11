f70ff9d713a5c9ebd497acb484aa846070336226
jaywezz <wawerujaymo92@gmail.com>
2024-04-11 03:35:26 -0700

    bug fix

--- lib/features/calender_events/repository/calender_events_repo.dart
+++ lib/features/calender_events/repository/calender_events_repo.dart
@@ -3,7 +3,9 @@ import 'dart:convert';
 
 // import 'package:http/http.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:monos/features/calender_events/models/calender_events_model.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 
 class CalenderEventsRepository {
@@ -16,17 +18,25 @@ class CalenderEventsRepository {
       receiveTimeout: const Duration(seconds: 60),
     );
 
-    final Dio dio = Dio(options);
+    final Dio dio = Dio(options)..interceptors.addAll([
+    // HttpFormatter(),
+    if (kDebugMode)
+      PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ),
+    ]);
+
     options.headers['Accept'] = 'application/json';
     options.headers['contentType'] = 'application/json';
-    Response response = await dio.get("/stockHistory");
+    Response response = await dio.get("/gToHCalendar/4/2024");
 
     if (response.statusCode == 200) {
-      final List result = jsonDecode(response.data)['data'];
+      final List result = response.data["data"];
       return result.map((e) => CalenderEventsModel.fromJson(e)).toList();
     } else {
       throw Exception("An error occurred");
     }
   }
-
 }
